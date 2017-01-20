3dd359a82080d4512e8d25035fec8bd18b2f0d2b
clement_lopez (DevLopez) <lopezclement31@gmail.com>
2017-01-20 21:08:47 +0100

    deadline

--- task-list-master/java/src/main/java/com/codurance/training/tasks/Deadline.java
+++ task-list-master/java/src/main/java/com/codurance/training/tasks/Deadline.java
@@ -1,5 +1,8 @@
 package com.codurance.training.tasks;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -18,19 +21,42 @@ public class Deadline implements Command {
    	 String[] subcommandRest = commandLine.split(" ", 2);         
         int id = Integer.parseInt(subcommandRest[0]);
         
-        for(int i=0; i<projects.size(); i++){
-        	Project project = projects.get(i);
-           for (Task task : project.getList()) {
-                if (task.getId() == id) {
-                    task.setDeadline(subcommandRest[1]);
-                    return projects;
-                }
+        String format = "dd/MM/yy";
+		SimpleDateFormat df = new SimpleDateFormat(format);
+		boolean pass = false;
+		try {
+            df.parse(subcommandRest[1]);
+            pass = true;
+        } catch (ParseException ex) {
+            System.out.println("Error! The correct date format is: " + format);
+        }
+		if(pass){
+	        for(int i=0; i<projects.size(); i++){
+	        	Project project = projects.get(i);
+	           for (Task task : project.getList()) {
+	                if (task.getId() == id) {
+	                    task.setDeadline(subcommandRest[1]);
+	                    return projects;
+	                }
+	                else if(task instanceof Project){
+	                	DeadlineInSubproject(((Project) task).getList(), id, subcommandRest[1]);
+	                }
+	            }
+	        }
+		}
+		return projects;
+	}
+
+	private void DeadlineInSubproject(ArrayList<Task> list, int id, String deadline) {
+		for (Task task : list) {
+            if (task.getId() == id) {
+                task.setDeadline(deadline);
+                return;
+            }
+            else if(task instanceof Project){
+            	DeadlineInSubproject(((Project) task).getList(), id, deadline);
             }
         }
-		
-		
-		
-		return projects;
 	}
 
 }
