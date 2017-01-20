ea489eff8ee93bc9fd586f6fcd00b9015bf150c2
clement_lopez (DevLopez) <lopezclement31@gmail.com>
2017-01-20 20:10:33 +0100

    ajout delete

--- task-list-master/java/src/main/java/com/codurance/training/tasks/Delete.java
+++ task-list-master/java/src/main/java/com/codurance/training/tasks/Delete.java
@@ -1,50 +1,53 @@
 package com.codurance.training.tasks;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Delete implements Command {
 
 	public List<Project> execute(String commandLine, List<Project> projects) {
 		int id = Integer.parseInt(commandLine);
-		//boolean test = false;
-    	for(int i=0; i<projects.size(); i++){
-    		Project project = projects.get(i);
-            for (Task task : project.getList()) {
+		boolean test1 = false, test2 = false;
+		Iterator<Project> iterProj = projects.iterator();
+		while(iterProj.hasNext()){
+			Project project = iterProj.next();
+			Iterator<Task> iterTask = project.getList().iterator();
+            while(iterTask.hasNext()){
+            	Task task = iterTask.next();
                 if (task.getId() == id) {
-                	project.getList().remove(task);
-                    //return projects;
+                	iterTask.remove();
+                	test1 = true;
                 }
                 else if(task instanceof Project){
-                	//test = DeleteInASubproject(id, ((Project) task).getList());
-    				/*if(test){
-    					return projects;
-    				}*/
-                	DeleteInASubproject(id, ((Project) task).getList());
+                	test2 = DeleteInASubproject(id, ((Project) task).getList());
                 }
             }
         }
-        System.out.printf("Could not find a task with an ID of %d.", id);
+		if ((!test1)&&(!test2)){
+			System.out.printf("Could not find a task with an ID of %d.", id);
+		}
         return projects;
 		
 	}
 
 	private boolean DeleteInASubproject(int id, ArrayList<Task> list) {
-		boolean test = false;
-		for(Task task : list){
+		boolean test1 = false, test2 = false;
+		Iterator<Task> iterTask = list.iterator();
+        while(iterTask.hasNext()){
+        	Task task = iterTask.next();
 			if (task.getId() == id) {
-				list.remove(task);
-				return true;
+				iterTask.remove();
+				test1 = true;
 			}
 			else if(task instanceof Project){
-				/*test = DeleteInASubproject(id, ((Project) task).getList());
-				if(test){
-					return true;
-				}*/
-				DeleteInASubproject(id, ((Project) task).getList());
+				test2 = DeleteInASubproject(id, ((Project) task).getList());
 			}
         }
-		return false;
+        if((test1==true)||(test2==true)){
+        	return true;
+        }
+        return false;
 	}
 
 	public static String HelpString() {
